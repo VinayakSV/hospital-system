0ad4cc75a477fe481c8f08317a006ec0494223f2
Vinayak S Varavate <vinayakvsvs@gmail.com>
2022-04-26 22:17:22 +0530

    Initial application structure

---
+++ hms/src/main/java/com/vcode/hms/HmsApplication.java
@@ -0,0 +1,13 @@
+package com.vcode.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsApplication.class, args);
+	}
+
+}
